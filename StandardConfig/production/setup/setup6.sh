f5eaa3cc600687c4ff4720c46875eb239f5d34d9
Victor Schwan <victor.schwan@cern.ch>
2025-04-15 14:54:02 +0200

    add new setup scripts

---
+++ StandardConfig/production/setup/setup6.sh
@@ -0,0 +1,30 @@
+# Function to print messages in specified color
+print_color() {
+    # ANSI yellow
+    local YELLOW='\033[1;33m'
+    # ANSI no color (reset)
+    local NC='\033[0m'
+    local PREFIX="[ setup ]:"
+    echo
+    echo -e "${YELLOW}${PREFIX}${NC} $1"
+    echo
+}
+
+print_color "k4n"
+k4n
+
+print_color "set myCodeDir env var"
+export myCodeDir="$HOME/promotion/code"
+
+print_color "set local k4geo"
+export k4geo_DIR="$myCodeDir/k4geo"
+export K4GEO="$k4geo_DIR"
+
+print_color "fix PYTHONPATH"
+export PYTHONPATH=$(pwd):$PYTHONPATH
+
+print_color "source env venv"
+source $myCodeDir/ILDConfig/StandardConfig/production/env/bin/activate
+
+print_color "add FCC Plotting Style"
+export PYTHONPATH=$PYTHONPATH:$HOME/promotion/code/
